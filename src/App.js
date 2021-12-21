60cad000a93d7932f46721c63713935bf34c3846
Darla Young <me3u2@hotmail.com>
2021-12-21 00:11:13 -0600

    installed firebase for signup/auth, private route setup complete, see README for use

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React, { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import ClassList from './components/ClassList';
+import { ProvideAuth } from './components/PrivateRoute';
 import WelcomeScreen from './components/WelcomeScreen';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<WelcomeScreen />} />
-      </Routes>
+      <Fragment>
+        <Routes>
+          <Route path='/' element={<WelcomeScreen />} />
+          <ProvideAuth>
+            <ClassList />
+          </ProvideAuth>
+        </Routes>
+      </Fragment>
     </div>
   );
 }
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect, useContext, createContext } from "react";
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAXWKE916Fa4VWNnsxu9mYvAJF9fQYJqqI",
+    authDomain: "anywhere-fitness-9.firebaseapp.com",
+    projectId: "anywhere-fitness-9",
+    storageBucket: "anywhere-fitness-9.appspot.com",
+    messagingSenderId: "557139415925",
+    appId: "1:557139415925:web:99394533fde24a763ef93c"
+});
+
+const authContext = createContext();
+
+export function ProvideAuth({ children }) {
+    const auth = useProvideAuth();
+    return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+};
+
+export const useAuth = () => {
+    return useContext(authContext);
+};
+
+function useProvideAuth() {
+    const [user, setUser] = useState(null);
+
+    const signin = (username, password) => {
+        return firebase
+        .auth()
+        .signInWithEmailAndPassword(username, password)
+        .then((response) => {
+            setUser(response.user);
+            return response.user;
+        });
+    };
+
+    const signup = (username, password) => {
+        return firebase
+        .auth()
+        .createUserWithEmailAndPassword(username, password)
+        .then((response) => {
+            setUser(response.user);
+            return response.user;
+        });
+    };
+
+    const signout = () => {
+        return firebase
+        .auth()
+        .signOut()
+        .then(() => {
+            setUser(false);
+        });
+    };
+    
+    useEffect(() => {
+        const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+          if (user) {
+            setUser(user);
+          } else {
+            setUser(false);
+          }
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return {
+        user,
+        signin,
+        signup,
+        signout
+    };
+}
