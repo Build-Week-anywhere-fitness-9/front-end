87bf2cf52d3a98e5dc526555ad9cb446f0edad8a
Darla Young <me3u2@hotmail.com>
2021-12-21 21:56:09 -0600

    updated with latest app.js from main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import WelcomeScreen from './Components/WelcomeScreen';
+import "./App.css";
+import React from "react";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import WelcomeScreen from "./components/WelcomeScreen";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<WelcomeScreen />} />
-      </Routes>
+      {/* header */}
+      <header className="App-header">
+        <h1>Anywhere Fitness</h1>
+        {/* nav links */}
+        <nav>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">SignUp</Link>
+        </nav>
+        {/* routes */}
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </header>
+
+      <Switch>
+        <Route exact path='/'>
+          <WelcomeScreen />
+        </Route>
+      </Switch>
     </div>
   );
 }
