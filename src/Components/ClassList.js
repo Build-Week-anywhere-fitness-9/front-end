e480ccdf042c7b7894d9c8d117664a5eb22300ae
Darla Young <me3u2@hotmail.com>
2021-12-22 03:58:48 -0600

    buttons are all functional

--- src/Components/ClassList.js
+++ src/Components/ClassList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 // import { getClasses } from '../actions';
 import { withRouter } from 'react-router-dom';
@@ -64,10 +64,26 @@ const ClassList = ({ user, classes, history, isFetching, error }) => {
     const otherClasses = [];
     
     classes.forEach(oneClass => {
-        if(user.username === oneClass.owner) {focusClasses.push(oneClass)}
+        if(user === oneClass.owner) {focusClasses.push(oneClass)}
         else if(oneClass.participants.includes(user)) {focusClasses.push(oneClass)}
         else {otherClasses.push(oneClass)}
     });
+
+    const handleClick = e => {
+        const target = classes.find(i => i.name === e.target.name);
+        const arr = target.participants;
+        if(arr.includes(user)) {
+            for (var i = 0; i < arr.length; i++) {
+                if(arr[i] === user ) {
+                    target.participants.splice(i, 1);
+                }
+            }
+        }
+        else {
+            target.participants.push(user);
+        };
+        history.push('/classlist');
+    };
 
     return (
         <div className='classList'>
@@ -84,41 +100,47 @@ const ClassList = ({ user, classes, history, isFetching, error }) => {
                         <li>Duration: {i.duration} minutes</li>
                         <li>Intensity Level: {i.intensity} out of 5</li>
                     </ul>
-                    {user.username === i.owner ?
+                    {user === i.owner ?
+                        /* owner options */
                         <div className='ownerOptions'>
                             {/* update push to actual edit class file name */}
-                            <button onclick={history.push.bind('/editClass')}>Edit Class</button>
+                            <button onClick={history.push.bind('/editClass')} className='editClass'>Edit Class</button>
                             <ul className='classParticipants'>
                                 {i.participants.map(p => (
                                     <li>{p}</li>
                                 ))}
                             </ul> 
-                        </div> : null
+                        </div> : 
+                        /* client options */
+                        <div className='clientOptions'>
+                            <button onClick={handleClick} name={i.name} className='reservation'>Withdraw</button>
+                        </div>
                     }
                 </div>
             ))}
             {otherClasses.map(i => (
-                <div className='otherClass'>
-                    <h3>{i.name}</h3>
-                    <ul className='genericClassList'>
-                        <li>Type: {i.type}</li>
-                        <li>Cost: {i.cost}</li>
-                        <li>Date: {i.date}</li>
-                        <li>Time: {i.time}</li>
-                        <li>Location: {i.location}</li>
-                        <li>Max size: {i.maxSize} participants</li>
-                        <li>Duration: {i.duration}</li>
-                        <li>Intensity Level: {i.intensity}</li>
-                    </ul>
-                </div>
-            ))}
+            <div className='otherClass'>
+                <h3>{i.name}</h3>
+                <ul className='genericClassList'>
+                    <li>Type: {i.type}</li>
+                    <li>Cost: ${i.cost}</li>
+                    <li>Date: {i.date}</li>
+                    <li>Time: {i.time}</li>
+                    <li>Location: {i.location}</li>
+                    <li>Max particpants: {i.maxSize}</li>
+                    <li>Duration: {i.duration} minutes</li>
+                    <li>Intensity Level: {i.intensity} out of 5</li>
+                </ul>
+                <button onClick={handleClick} name={i.name} className='reservation'>Attend Class</button>
+            </div>
+        ))}
         </div>
-    )
+    );
 };
 
 const mapStateToProps = state => {
     return {
-        user: /*state.user,*/ user,
+        user: /*state.user,*/ user.username,
         classes: /*state.classes*/ classes,
         isFetching: state.isFetching,
         error: state.error
@@ -135,6 +157,3 @@ export default connect(mapStateToProps)(withRouter(ClassList));
 //     - type
 //     - intensity
 //     - location
-// add buttons:
-//     - client: make/cancel reservation
-//     - instructor: edit class
