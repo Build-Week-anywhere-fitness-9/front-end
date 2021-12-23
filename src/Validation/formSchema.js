b79a07b9a3df91c698715d2b6c891337e4f2fae6
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2021-12-23 17:35:51 -0600

    Update formSchema.js

--- src/Validation/formSchema.js
+++ src/Validation/formSchema.js
@@ -1,15 +1,15 @@
 import * as yup from "yup";
 
 const formSchema = yup.object().shape({
-  className: yup
+  name: yup
     .string()
     .trim()
     .required("Class name is required.")
     .min(5, "Class name must be at least 5 characters.")
     .max(30, "Class name must be no more than 30 characters."),
-  startTime: yup.string().required("Pick a start time."),
-  classDate: yup.string().required("Pick a date."),
-  classType: yup.string().required("A class type is required."),
+  time: yup.string().required("Pick a start time."),
+  date: yup.string().required("Pick a date."),
+  type: yup.string().required("A class type is required."),
   duration: yup.string().required("Class duration is required."),
   intensity: yup.number(),
   location: yup
@@ -17,7 +17,7 @@ const formSchema = yup.object().shape({
     .trim()
     .required("A location is required.")
     .min(6, "Your location must be at least 6 characters long."),
-  maxCapacity: yup
+  maxSize: yup
     .number()
     .required("Pick a class capacity.")
     .min(1, "Capacity must be at least 1."),
